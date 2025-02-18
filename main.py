a038919d6e7b45b4c417d86d6ef98d5ce31ced76
Jay <jp6631@srmist.edu.in>
2025-02-18 14:39:35 +0530

    Scraping the data from amazon

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ from bs4 import BeautifulSoup
 URL="https://appbrewery.github.io/instant_pot/"
 response=requests.get(URL)
 soup = BeautifulSoup(response.text, 'html.parser')
+o_price=soup.find(class_="a-offscreen")
+price=o_price.text.split("$")[1]
+
+
