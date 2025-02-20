62d1278ba2cd05ee1239b6efbbcdc7dd07ed00e8
Jay <jp6631@srmist.edu.in>
2025-02-20 14:35:56 +0530

    Adding headers

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ my_email=os.getenv("my_email")
 password=os.getenv("password")
 
 
-URL="https://appbrewery.github.io/instant_pot/"
-response=requests.get(URL)
+URL="https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
+response=requests.get(URL,headers={"Accept-Language":"en-IN","User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"})
 soup = BeautifulSoup(response.text, 'html.parser')
 o_price=soup.find(class_="a-offscreen")
 price=float(o_price.text.split("$")[1])
